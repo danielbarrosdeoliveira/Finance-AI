111ce2387b434aa72bd389664284209997d73dd8
Daniel Barros de Oliveira <danielbarrosdeoliveira@outlook.com>
2024-11-18 23:51:33 -0300

    feat: add loggedin validation on routes

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,14 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 const SubscriptionPage = ({}) => {
-  return <div></div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return <h1>Subscription</h1>;
 };
 
 export default SubscriptionPage;

6d777aae6cc35ba5bea379918170585c854b2540
Daniel Barros de Oliveira <danielbarrosdeoliveira@outlook.com>
2024-11-25 22:51:07 -0300

    feat: addstripe webhook that handles subscription

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,16 +1,21 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import AcquirePlanButton from "./_componentes/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
-const SubscriptionPage = ({}) => {
+const SubscriptionPage = async ({}) => {
   const { userId } = auth();
 
   if (!userId) {
     redirect("/login");
   }
+
+  const user = await clerkClient().users.getUser(userId);
+
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <div className="py6 space-y-6 p-6 px-8">
@@ -40,13 +45,20 @@ const SubscriptionPage = ({}) => {
               <p>Relatórios de IA</p>
             </div>
           </CardContent>
-          <Button variant="outline" className="w-full rounded-full font-bold">
-            Fazer Upgrade
-          </Button>
+          <div className="px-4">
+            <Button variant="outline" className="w-full rounded-full font-bold">
+              Fazer Upgrade
+            </Button>
+          </div>
         </Card>
 
         <Card className="w-[450px]">
-          <CardHeader className="border-b border-solid py-8">
+          <CardHeader className="relative border-b border-solid py-8">
+            {hasPremiumPlan && (
+              <Badge className="absolute left-4 top-6 bg-primary/10 text-primary">
+                Atual
+              </Badge>
+            )}
             <h2 className="text-center text-xl">Plano Premium</h2>
             <div className="flex items-center justify-center">
               <div className="flex items-center justify-center gap-3">
@@ -58,7 +70,7 @@ const SubscriptionPage = ({}) => {
               </div>
             </div>
           </CardHeader>
-          <CardContent className="space-y-6 py-8">
+          <CardContent className="space-y-6 px-4 py-8">
             <div className="flex items-center gap-2">
               <CheckIcon className="text-primary" />
               <p>Transações Ilimitadas</p>
@@ -68,7 +80,9 @@ const SubscriptionPage = ({}) => {
               <p>Relatórios de IA</p>
             </div>
           </CardContent>
-          <AcquirePlanButton />
+          <div className="px-4">
+            <AcquirePlanButton />
+          </div>
         </Card>
       </div>
     </div>
